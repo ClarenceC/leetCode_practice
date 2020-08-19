3a8bbfbd609e6b19d06e60d62ca76cc24a15d690
ClarenceChan <tong901@163.com>
2020-08-19 15:43:07 +0800

    242.有效的字母异位词

---
+++ 242.有效的字母异位词.js
@@ -0,0 +1,30 @@
+// 242.有效的字母异位词
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  const arr1 = s.split('')
+  const arr2 = t.split('')
+  let matchIndex
+  if (arr1.length !== arr2.length) { return false }
+  for (let i = 0; i < arr1.length; i++) {
+      matchIndex = arr2.findIndex(item => item === arr1[i])
+      if (matchIndex < 0) {
+          break
+      } else {
+          arr2.splice(matchIndex, 1)
+      }
+  }
+  if (arr2.length === 0) {
+      return true
+  } else {
+      return false
+  }
+};
+
+// Run
+
+var s = '', 
